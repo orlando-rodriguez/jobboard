85a87acbf9a3e57ff4829b03ab671a663097b167
Marlena Baker <baker.marlena@gmail.com>
2018-01-08 11:35:27 -0700

    finished versio 3

---
+++ drills/version-3/apply.js
@@ -0,0 +1,36 @@
+(function initializePage(){
+  document.querySelector('#application-text').addEventListener('keyup', updatePreview);
+  document.querySelector('#preview-toggle').addEventListener('click', togglePreview);
+  getListing().then(response => {
+    addListing(response);
+  });
+})();
+
+function getListing() {
+  let apiURL = './listing.json';
+  return fetch(`${apiURL}`).then(response => response.json());
+}
+
+function addListing(data) {
+  const container = document.querySelector('#job-details');
+  container.append(makeElement('h4', data.title));
+  container.append(makeElement('p', data.description));
+}
+
+
+function makeElement(tag, text){
+  let element = document.createElement(tag);
+  element.textContent = text;
+  return element;
+}
+
+function updatePreview() {
+  const preview = document.querySelector('#application-preview');
+  const text = document.querySelector('#application-text').value;
+  preview.textContent = text;
+}
+
+function togglePreview() {
+  const preview = document.querySelector('#application-preview');
+  preview.classList.toggle('hidden');
+}
