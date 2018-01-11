c266c201274d85bb1c4e907619bba8fe921679b1
Marlena Baker <baker.marlena@gmail.com>
2018-01-11 11:23:43 -0700

    finished v2 solution

---
+++ drills/version-2/app/src/components/SkillsLists.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id='SkillsLists'>
+    <li v-for="skill in skills" :key="skill">{{skill}}</li>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SkillsLists',
+  props: {
+    skills: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ drills/version-2/app/src/components/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id='TheFooter'>
+    <small>Copyright 2018</small>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TheFooter'
+}
+</script>
+<style scoped>
+
+  TheFooter {
+    position: fixed;
+    bottom: 0;
+    padding: 5px;
+  }
+
+  small {
+    margin: 0 auto;
+  }
+
+</style>
