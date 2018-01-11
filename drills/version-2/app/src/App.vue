9f5590b811630ad748e2f25f0e8e1f353f720cb5
Marlena Baker <baker.marlena@gmail.com>
2018-01-11 11:23:43 -0700

    finished v2 solution

---
+++ drills/version-2/app/src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <TheHeader></TheHeader>
+    <main>
+      <ProfileCards v-for="profile in profiles" :key="profile.name" :profile="profile"></ProfileCards>
+    </main>
+    <TheFooter></TheFooter>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader'
+import TheFooter from './components/TheFooter'
+import ProfileCards from './components/ProfileCards'
+import SkillsLists from './components/SkillsLists'
+
+export default {
+  name: 'app',
+  components: {
+    TheHeader,
+    TheFooter,
+    ProfileCards,
+    SkillsLists
+  },
+  data () {
+    return {
+      profiles: [],
+      apiURL: '../static/dinosaurs.json'
+    }
+  },
+  mounted () {
+    fetch(this.apiURL)
+      .then(response => response.json())
+      .then(response => {
+        this.profiles = response
+      })
+  }
+}
+</script>
+
+<style>
+html {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin: 0 30px 0 30px;
+  padding: 0;
+  font-family: sans-serif;
+  color: #1B997A;
+  display: grid;
+  grid-template-rows: 15% 75% 10%;
+}
+</style>
---
+++ drills/version-2/app/src/components/ProfileCards.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id='ProfileCards'>
+    <header class="profile-header" @click="skillsToggle = !skillsToggle">
+      <img :src='profile.image' />
+      <h2>{{profile.name}}</h2>
+    </header>
+    <div class='skills-container' v-if='skillsToggle'>
+      <ul class='skills-list'>
+        <h4>Skills</h4>
+        <SkillsLists :skills='profile.skills'></SkillsLists>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import SkillsLists from './SkillsLists'
+
+export default {
+  name: 'ProfileCards',
+  components: {
+    SkillsLists
+  },
+  props: {
+    profile: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      skillsToggle: false
+    }
+  }
+}
+</script>
+<style scoped>
+
+  #ProfileCards {
+    border-radius: 6px;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.3);
+    padding: 10px;
+    /* margin: 0 auto 10px auto; */
+  }
+
+  .profile-header {
+    display: flex;
+    flex-flow: row nowrap;
+  }
+
+  .profile-header img {
+    width: 100px;
+    height: 100px;
+    border: 1px solid darkgrey;
+    margin: 0 10px 0 10px;
+  }
+
+  .skills-list {
+    list-style: none;
+  }
+
+</style>
